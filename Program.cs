ebe2cdb907efe9717fdf63d00d3a317639fefea5
zhannulka <124399184+zhannulka@users.noreply.github.com>
2023-02-19 09:48:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+ { System.Console.WriteLine("Число введено корректно"); }
+else { System.Console.WriteLine($"Число {number} не являеться трёхзначным"); }
+int first = number % 100;
+int second = first / 10;
+System.Console.WriteLine($"Вторая цифра числа {second}");
+
+
